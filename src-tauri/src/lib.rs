909ef9f7b34a00d8e30d3ce9d03b713479bb4fac
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-01-11 11:32:01 +0800

    添加日志插件，更新配置文件以支持日志功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,11 @@ mod db;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .level(tauri_plugin_log::log::LevelFilter::Info)
+                .build(),
+        )
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
