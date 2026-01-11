d4da9982616f2140d71503a516d10fcd39dc7c26
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-01-11 17:49:33 +0800

    新增样式文件，优化应用界面

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from "./router";
 import {Dialog, Notify, Quasar} from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
+import './style.css'
 
 createApp(App)
     .use(router)
