823078a255a90d02d8caaddc443e8653a6510e80
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-01-11 13:39:35 +0800

    删掉没用到的pinia

--- src/stores/useFitnessStore.ts
+++
@@ -1,7 +0,0 @@
-import {defineStore} from "pinia";
-import {ref} from "vue";
-import {Routine} from "../types.ts";
-
-export const useFitnessStore = defineStore('fitness', () => {
-    const routines = ref<Routine[]>([]);
-})
