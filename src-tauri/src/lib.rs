5470f4dcd180c2acd2408f7ec5eb90a3fc260c28
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-03-18 00:02:18 +0800

    去掉process插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -249,7 +249,6 @@ async fn import_from_downloads(
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(
             tauri_plugin_log::Builder::new()
