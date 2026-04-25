27a94c156603c2b21ce16239660bb39c9bb580e2
SurKaa <98200894+surkaa@users.noreply.github.com>
2026-04-25 11:12:18 +0800

    chore: simplify calendar summary copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
       <div>
         <div class="text-caption text-grey-7">月视图</div>
         <div class="text-subtitle1 text-weight-medium">
-          {{ activeDayCount }} 天有训练，状态按当天动作数分档
+          {{ activeDayCount }} 天有训练
         </div>
       </div>
 
